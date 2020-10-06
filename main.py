bb1cf6029ba3a309bf3eeed9bf2f5f136f9331ce
Dat Ma XM <datma113112111@gmail.com>
2020-10-06 20:14:45 +0700

    update: some UI design

--- main.py
+++ main.py
@@ -56,7 +56,30 @@ main = html.Div([
             ], className='row', style={ 'fontSize': '15px' })
         ])
     ], className='mt-5 bg-light intro'),
+     html.Div([
+        html.Div([
+            html.Div('About my team:', style={
+                'fontSize': '25px',
+                'fontWeight': 'bold',
+            }),
+            html.Div([
+                html.Img(src='./assets/team.jpg', className='col-4'),
+                html.Div([
+                html.Div('18081331 - Nguyễn Công Thành Đạt'),
+                html.Div('18089811 - Mai kiên cường'),
+                html.Div('18084791 - Trương Công Cường'),
+                html.Div('18079251 - Đỗ Tùng Dương'),   
+                ],className='col-4 team'),
+                 html.Div( [
+                html.Div('18092791 - Hoàng Hữu Huy'),
+                html.Div('18084851 - Châu Quốc An'),
+                html.Div('18072661 - Lại Văn Vượng')       
+                ],className='col-4'),
+            ], className='row', style={ 'fontSize': '15px' })
+        ])
+    ], className='mt-5 bg-light intro'),
 
+    #my team
     # background
     html.Div(style={
         'width': '100%',
@@ -68,13 +91,62 @@ main = html.Div([
     }, className='bg-dark')
 ], className='container mt-5')
 
+##-----------------------------------------------------
+#Simple Chart Link
+simpleChart = html.Div([
+     html.Div(style={
+        'width': '100%',
+        'height': '100%',
+        'position': 'absolute',
+        'top': '0',
+        'left': '0',
+        'z-index': '-999'
+    }, className='bg-dark'),
+     # home page text
+    html.Div('This is project of our team with Dash - plotly ', style={
+        'height': '50px',
+        'width': '100%',
+        'backgroundColor': '#f5f5f5',
+        'paddingLeft': '25px',
+        'position': 'absolute',
+        'top': '0',
+        'left': '0',
+        'display': 'flex',
+        'alignItems': 'center'
+    }),
+    html.Div([
+        html.Div([
+            html.Ul([
+                dcc.Link('Matplotlib', href="/simple-chart", className='el'),
+                dcc.Link('Line Chart', href="/line-chart", className='el'),
+                dcc.Link('Bar Chart', href="/bar-chart", className='el'),
+                dcc.Link('Pie Chart', href="/pie-chart", className='el'),
+                dcc.Link('Scatter Chart', href="/scatter-chart", className='el'),
+            ],className='listInside')
+        ],
+        className='col-3 listContainer bg-light'),
+        html.Div([
+            html.Div([
+                html.Div('Matplotlib', className='title'),
+                dcc.Link('Home Page', href="/"),
+            ], className='fl'),
+            html.Div([
+                 html.Span('Introducing to matplotlib:', className='introMatplotlib'),
+                html.Span('asdasdasd asdasdasd asdasdasd asdasdasd asdasdasd asdasdasd asdasdasd asdasdasd ',className='content')
+            ])
+           
+        ],className='col-8 matplotlib bg-light'),
+    ], className = 'row cc')
+], className='container cc')
+
+##-----------------------------------------------------
 # and this code to transfer to another link
-
-
 @app.callback(dash.dependencies.Output('page-content', 'children'),
               [dash.dependencies.Input('url', 'pathname')])
 def display_page(pathname):
-    if pathname == '/':
+    if pathname == '/simple-chart':
+        return simpleChart
+    else:
         return main
 #     elif pathname == '/page2':
 #         return page2
